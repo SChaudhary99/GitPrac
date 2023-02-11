bf3b2b44fd438e1bede79f60a74468372a698124
Soham Chaudhary <sohamamrit@gmail.com>
2023-02-11 10:11:19 -0500

    Soham method

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,9 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Soham();
+    }
+
+    public static void Soham(){
+        System.out.println("This is soham's method!");
     }
 }
